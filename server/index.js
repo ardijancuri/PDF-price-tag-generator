971b565bb5d872b98bcb6d9f523861ecbc1ad539
Ardijan Curi <ardijancuri3@hotmail.com>
2025-11-30 15:48:21 +0100

    Change text color from black to #373435 (dark gray) and switch to FuturaCyrillicDemi

--- server/index.js
+++ server/index.js
@@ -47,15 +47,15 @@ app.post('/api/generate-pdf', async (req, res) => {
         // Load Futura Cyrillic fonts (better support for Cyrillic characters)
         const futuraBookPath = join(__dirname, 'fonts', 'FuturaCyrillicBook.ttf')
         const futuraHeavyPath = join(__dirname, 'fonts', 'FuturaCyrillicHeavy.ttf')
-        const futuraMediumPath = join(__dirname, 'fonts', 'FuturaCyrillicMedium.ttf')
+        const futuraDemiPath = join(__dirname, 'fonts', 'FuturaCyrillicDemi.ttf')
 
         const futuraBookBytes = await readFile(futuraBookPath)
         const futuraHeavyBytes = await readFile(futuraHeavyPath)
-        const futuraMediumBytes = await readFile(futuraMediumPath)
+        const futuraDemiBytes = await readFile(futuraDemiPath)
 
         const regularFont = await pdfDoc.embedFont(futuraBookBytes)
         const boldFont = await pdfDoc.embedFont(futuraHeavyBytes)
-        const mediumFont = await pdfDoc.embedFont(futuraMediumBytes)
+        const mediumFont = await pdfDoc.embedFont(futuraDemiBytes)
 
         // Get the first page
         const pages = pdfDoc.getPages()
@@ -81,24 +81,27 @@ app.post('/api/generate-pdf', async (req, res) => {
          *   - Field 5: Product code
          *   - Field 6: Dimensions
          */
+        // Convert hex color #373435 to RGB (55, 52, 53)
+        const textColor = rgb(55 / 255, 52 / 255, 53 / 255) // #373435
+
         const fieldPositions = [
             // Field 1 - Discount "40%" - HUGE bold WHITE text, top center of page
             { x: 80, y: height - 250, fontSize: 260, font: boldFont, color: rgb(1, 1, 1) }, // White text
 
             // Field 2 - Product name "КЕБЕ СО ДЕЗЕН" - Bold, large, centered
-            { x: 80, y: height - 520, fontSize: 70, font: mediumFont, color: rgb(0, 0, 0) },
+            { x: 80, y: height - 520, fontSize: 70, font: mediumFont, color: textColor },
 
             // Field 3 - Original price "800,-" - Bold, right side after МКД label
-            { x: 80, y: height - 650, fontSize: 120, font: boldFont, color: rgb(0, 0, 0) },
+            { x: 80, y: height - 650, fontSize: 120, font: boldFont, color: textColor },
 
             // Field 4 - Discounted price "480,-" - Bold, right side after МКД label
-            { x: 80, y: height - 840, fontSize: 230, font: boldFont, color: rgb(0, 0, 0) },
+            { x: 80, y: height - 840, fontSize: 230, font: boldFont, color: textColor },
 
             // Field 5 - Product code "246403" - Regular weight, bottom left
-            { x: 80, y: 280, fontSize: 50, font: mediumFont, color: rgb(0, 0, 0) },
+            { x: 80, y: 280, fontSize: 50, font: mediumFont, color: textColor },
 
             // Field 6 - Dimensions - Regular weight, below product code
-            { x: 80, y: 220, fontSize: 40, font: regularFont, color: rgb(0, 0, 0) },
+            { x: 80, y: 220, fontSize: 40, font: regularFont, color: textColor },
         ]
 
         // Draw each field on the PDF
